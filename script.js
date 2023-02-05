229d6971ad56a5203c2de7ed6183bcf31d32f213
josh612 <joshamy@gmail.com>
2023-02-05 14:50:44 +0000

    Create a formData object and add updates to it from the event listeners

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const form = document.querySelector('#signup-form');
-const creditCard = document.querySelector('#cc');
+const creditCardInput = document.querySelector('#cc');
 const termsCheckbox = document.querySelector('#terms');
 const veggieSelect = document.querySelector('#veggie');
 
+const formData = {};
 
-form.addEventListener('submit', function(e){
-	e.preventDefault();
-	console.log('cc', creditCard.value);
-	console.log('terms', termsCheckbox.checked);
-	console.log('veggie', veggieSelect.value);
-	
+creditCardInput.addEventListener('input', (e) => {
+	console.log('CC Changed!', e);
+	formData['cc'] = e.target.value;
 });
+
+veggieSelect.addEventListener('input', (e) => {
+	console.log('Veggie Changed!', e);
+	formData['veggie'] = e.target.value;
+});
+
+termsCheckbox.addEventListener('change', (e) => {
+	console.log('Terms Changed!', e);
+	formData['terms'] = e.target.checked;
+}
+
+// form.addEventListener('submit', function(e){
+// 	e.preventDefault();
+// 	console.log('cc', creditCard.value);
+// 	console.log('terms', termsCheckbox.checked);
+// 	console.log('veggie', veggieSelect.value);
+	
+// });
