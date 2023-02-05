016c0456e3e9415b7e1b2187aaf532c4b8bff3d1
josh612 <joshamy@gmail.com>
2023-02-05 15:43:53 +0000

    Refactored code to accept generic input types

--- script.js
+++ script.js
@@ -5,20 +5,28 @@ const veggieSelect = document.querySelector('#veggie');
 
 const formData = {};
 
-creditCardInput.addEventListener('input', (e) => {
-	console.log('CC Changed!', e);
-	formData['cc'] = e.target.value;
-});
-
-veggieSelect.addEventListener('input', (e) => {
-	console.log('Veggie Changed!', e);
-	formData['veggie'] = e.target.value;
-});
-
-termsCheckbox.addEventListener('change', (e) => {
-	console.log('Terms Changed!', e);
-	formData['terms'] = e.target.checked;
+for (let input of [creditCardInput, termsCheckbox, veggieSelect]) {
+	input.addEventListener('input', ({target}) => {
+		const {name, type, value, checked} = target;
+		formData[name] = type === 'checkbox' ? checked : value;
+		console.log(formData);
+	});
 }
+
+// creditCardInput.addEventListener('input', (e) => {
+// 	console.log('CC Changed!', e);
+// 	formData['cc'] = e.target.value;
+// });
+
+// veggieSelect.addEventListener('input', (e) => {
+// 	console.log('Veggie Changed!', e);
+// 	formData['veggie'] = e.target.value;
+// });
+
+// termsCheckbox.addEventListener('change', (e) => {
+// 	console.log('Terms Changed!', e);
+// 	formData['terms'] = e.target.checked;
+// });
 
 // form.addEventListener('submit', function(e){
 // 	e.preventDefault();
